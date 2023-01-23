e4f8f4a084fb6f5bcf4ecf95f4a5fbefd5dc7ecf
yaroslavsf <yaroslav1598753@gmail.com>
2023-01-23 09:06:38 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card } from './components/Card';
+import { Form } from './components/Form';
+
+function App() {
+  return (
+    <>
+      <Card/>
+      <Form/>
+    </>
+  );
+}
+
+export default App;
